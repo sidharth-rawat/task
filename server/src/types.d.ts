4effb85bdde3c71548755d9e9049e553b4a5a1aa
Sidharth Rawat <whosidrawat69@gmail.com>
2023-04-19 14:31:26 +0530

    type changes

--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -1,41 +1,6 @@
 
 import mongoose = require('mongoose')
 
-export type IEnvironmentType = 'production' | 'staging' | 'development' | 'beta';
-
-export interface IPopulate {
-  path: string;
-  model?: string;
-  populate?: IPopulate;
-}
-
-export interface IPagination {
-  perPage?: number,
-  pageNo?: number,
-}
-
-
-export interface IFindPaginationQuery{
-  page_size?: number,
-  page_number?:number
-}
-
-export interface IEnv {
-  stage?: string;
-  port: number;
-  db: IMongoDBCfg;
-  domain: string;
-  apiPath: string;
-  staticPath: string;
-}
-
-export interface IMongoDBCfg {
-  name: string;
-  user: string;
-  pw: string;
-  account: string;
-  uri: (user: string, pw: string, name: string, account: string) => string;
-}
 
 export interface IModel {
     mongooseModel: mongoose.Model<any>;
